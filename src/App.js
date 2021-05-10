f5442027c525c78725100e69c1af565c94a9a433
SMakaiTakori <makait87@icloud.com>
2021-05-10 15:10:11 -0400

    set up react-markdown app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import "./App.css";
 
-function App() {
+export const App = () => {
+  const [input, setInput] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        className="textarea"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+
+      <ReactMarkdown source={input} className="markdown" />
     </div>
   );
-}
+};
 
 export default App;
