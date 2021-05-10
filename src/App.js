d9e38c34c07bfff0d1a4a45c3777d28106dfe921
SMakaiTakori <makait87@icloud.com>
2021-05-10 15:54:04 -0400

    added rehypeRaw so I can use a mix of markdown and html to render on the DOM

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import gfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
 import "./App.css";
 
 export default function App() {
-  const markdown = `Just a link: https://reactjs.com.`;
+  const markdown = `<div class="note">
+
+Some *emphasis* and <strong>strong</strong>!
+`;
 
   return (
     <div className="App">
       <ReactMarkdown
         remarkPlugins={[gfm]}
+        rehypePlugins={[rehypeRaw]}
         children={markdown}
         className="markdown"
       />
